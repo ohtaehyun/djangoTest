5f65e77a726488a3d6a58734c0017b45f2f5e2f0
ohtaehyun <othdev95@gmail.com>
2019-08-08 10:12:59 +0900

    add some urls and template

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.post_list, name='post_list'),
+    path('',views.about_me, name='about_me'),
+    path('about_me',views.about_me, name='about_me'),
+    path('project', views.project, name='project'),
+    path('post_list',views.post_list, name='post_list'),
     path('post/<int: pk>/', views.post_detail, name='post_detail'),
+    path('hi_ramji', views.hi_ramji, name='hi_ramji')
     
 ]
