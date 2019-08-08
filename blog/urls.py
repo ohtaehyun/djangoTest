02c2baf8e0c58045c0ba07ca21ac0dd8c7070ac6
ohtaehyun <othdev95@gmail.com>
2019-08-08 15:51:54 +0900

    post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ urlpatterns=[
     path('about_me',views.about_me, name='about_me'),
     path('project', views.project, name='project'),
     path('post_list',views.post_list, name='post_list'),
-    path('post/<int: pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('hi_ramji', views.hi_ramji, name='hi_ramji')
     
 ]
